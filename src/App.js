04443baa8005dbaf230dfeec7fad0bf002a2f5a5
ankitkadyan06 <ankit.kadyan@designx.in>
2024-10-17 18:18:10 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
-// import Brochure from './Pages/Brochure';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/QR">
     <Routes>
       <Route exact path="/" element={<Homepage/>}/>
-      {/* <Route path="/brochure" element={<Brochure/>}/> */}
     </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
--- src/Components/Homepage/Content.js
+++ src/Components/Homepage/Content.js
@@ -4,13 +4,13 @@ import web from "../Assets/Images/web.svg";
 import android from "../Assets/Images/android.svg";
 import brochure from "../Assets/Images/brochure.svg";
 import { Link } from "react-router-dom";
-import globe from '../Assets/Images/globe.svg';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import brochurePdf from '../Assets/Pdf/Df-OS Lighthouse.pdf';
-import '../Assets/CSS/PdfBooklet.css';
-import close from '../Assets/Images/close.svg';
-import download from '../Assets/Images/download.svg';
+import globe from "../Assets/Images/globe.svg";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import brochurePdf from "../Assets/Pdf/Df-OS Lighthouse.pdf";
+import "../Assets/CSS/PdfBooklet.css";
+import close from "../Assets/Images/close.svg";
+import download from "../Assets/Images/download.svg";
 
 const Content = () => {
   const [selectedPdf, setSelectedPdf] = useState(null);
@@ -40,16 +40,32 @@ const Content = () => {
       </div>
       <div className="flex justify-between md:justify-end mt-[20px]">
         <Link to="https://apps.apple.com/us/app/techfiles/id1458479861">
-          <img src={ios} alt="" className="w-[90px] md:w-[97px] h-[47px] md:h-[57px]" />
+          <img
+            src={ios}
+            alt=""
+            className="w-[90px] md:w-[97px] h-[47px] md:h-[57px]"
+          />
         </Link>
         <Link to="https://dfos.co/app/login">
-          <img src={web} alt="" className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px]" />
+          <img
+            src={web}
+            alt=""
+            className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px]"
+          />
         </Link>
         <Link to="https://play.google.com/store/search?q=DFOS&c=apps">
-          <img src={android} alt="" className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px]" />
+          <img
+            src={android}
+            alt=""
+            className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px]"
+          />
         </Link>
         <div onClick={() => openPdfModal("brochure.pdf")}>
-          <img src={brochure} alt="" className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px] cursor-pointer" />
+          <img
+            src={brochure}
+            alt=""
+            className="ml-0 md:ml-[20px] w-[90px] md:w-[97px] h-[47px] md:h-[57px] cursor-pointer"
+          />
         </div>
       </div>
       <img src={globe} alt="" className="absolute left-0 bottom-0" />
@@ -64,13 +80,18 @@ const Content = () => {
             >
               <img src={close} alt="Close" className="w-[30px] h-[30px]" />
             </button>
-            <Link
-              to={brochurePdf}
+            <a
+              href={brochurePdf}
               target="_blank"
+              rel="noopener noreferrer"
               className="absolute bottom-2 right-2 z-[100] text-black text-lg"
             >
-              <img src={download} alt="Download" className="w-[40px] h-[40px]" />
-            </Link>
+              <img
+                src={download}
+                alt="Download"
+                className="w-[40px] h-[40px]"
+              />
+            </a>
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
               <Viewer fileUrl={selectedPdf} />
             </Worker>
